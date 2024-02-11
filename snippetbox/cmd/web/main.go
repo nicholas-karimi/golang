eadd481e23825249448dbac36164638e04cdde05
nicholas-karimi <kariminic@gmail.com>
2024-02-11 22:32:13 +0300

    levelled logging using INFO & ERROR - StdOut/Stederr

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
-	"flag"
+	"os"
 
 	"github.com/nicholas-karimi/snippetbox/cmd/web/handlers"
 )
@@ -14,6 +15,14 @@ func main() {
 
 	// parse the commabndline flag
 	flag.Parse()
+
+	// use log.New() to create a logger for writing information messages.
+	// takes 3 params: destination to write logs(os.Stdout), string prefix for message(INFO)
+	//and tags for additional ifno to include (local date and time - flags joined using bitwise OR operator |)
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// error msg logger - use stderr as destination and log.Lshortfile flag to include relevant filebame and line number
+	errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -29,10 +38,12 @@ func main() {
 	mux.HandleFunc("/snippet/create", handlers.SnippetCreate)
 
 	// log.Print("Starting web server on :4000")
-	log.Printf("Starting server on %s", *addr)
+	// log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 
 	// err := http.ListenAndServe(":4000", mux)
 	err := http.ListenAndServe(*addr, mux)
-	
-	log.Fatal(err)
+
+	// log.Fatal(err)
+	errLog.Fatal(err)
 }
